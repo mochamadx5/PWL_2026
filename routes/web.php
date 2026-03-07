3339e79d2e7529182b3f3ce469299823d9949ffc
mochamadx5 <mochamadrezafirdaus4@gmail.com>
2026-03-07 22:48:29 +0700

    multi_parameters

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,12 @@ use Illuminate\Support\Facades\Route;
     Route::get('/user/{name}', function ($name) {
         return 'Nama saya, ' . $name;
     });
+
+    Route::get('/posts/{post}/comments/{comment}',
+        function ($postId, $commentId) {
+        return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+    });
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
